d43f4c1fbe1b0bf9fe9fd592591d9d793e2fb343
Samuel Wellander <samwellander@gmail.com>
2017-04-04 16:03:17 -0700

    Add comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,4 +7,12 @@ class CommentsController < ApplicationController
     redirect_to post_path(@post)
   end
 
+  def destroy
+    @post = Post.find(params[:post, :id])
+    @comment = @post.comment.find(params[:id])
+    @comment.destroy
+
+    redirect_to post_path(@post)
+  end
+
 end
